a4e3a4dd5589d7bab8b826374499bf83f2eea59c
Manjula-g475 <manjulag475@gmail.com>
2024-02-15 16:08:13 +0530

    Initial commit

---
+++ Java_Feb/src/Example2/Datatypes.java
@@ -0,0 +1,19 @@
+package Example2;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 12;
+		short s = 20;
+		int i = 30;
+		long l = 125l;
+		double d = 2987d;
+		float f = 234.4f;
+		char ch = 'A';
+		boolean bl = true;
+		System.out.println("Primitive datatypes are:" +b + " " +s + " "+i +" "+l + " "+d +" "+f +" "+ch +" "+bl);
+
+	}
+
+}
